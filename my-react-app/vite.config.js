4a4c52814db5c0709ce4a87320484c3c91837463
Abigael Ngalu <abigaelngalu@gmail.com>
2025-11-13 13:00:03 +0000

    update changes

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',   // Exposes server to Codespaces
-    port: 5174,        // Changed port to 5174
+    port: 5173,        // Changed port to 5174
     strictPort: true   // Prevents auto-switching to another port
   },
 })
