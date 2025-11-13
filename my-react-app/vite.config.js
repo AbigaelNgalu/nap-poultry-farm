13f22b86fc7a8286cb041f833268bd8751e72acd
Abigael Ngalu <abigaelngalu@gmail.com>
2025-11-13 12:33:28 +0000

    update changes

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',   // 👈 important: explicitly bind to all interfaces
-    port: 5173,
-    strictPort: true
+    host: '0.0.0.0',   // 👈 required for Codespaces
+    port: 5173,        // 👈 force to use this port
+    strictPort: true   // 👈 don’t switch to 5174 automatically
   },
 })
+
 
 
