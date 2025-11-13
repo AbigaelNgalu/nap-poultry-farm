b029055026f068ec02b0f3cb17349bc4ac3f22bd
Abigael Ngalu <abigaelngalu@gmail.com>
2025-11-13 12:45:19 +0000

    update changes

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+// App.jsx
 import React from "react";
 import "./App.css";
 import Navigation from "./components/layout/Navigation";
@@ -11,58 +12,66 @@ import Stats from "./components/sections/Stats";
 import Testimonials from "./components/sections/Testimonials";
 import Newsletter from "./components/sections/Newsletter";
 
+// Optional: If you want to wrap everything in HashRouter
+import { HashRouter } from "react-router-dom";
+
 function App() {
   return (
-    <div className="flex flex-col min-h-screen bg-gray-50 text-gray-900">
-      {/* Navigation Bar */}
-      <Navigation />
+    <HashRouter>
+      <div className="flex flex-col min-h-screen bg-gray-50 text-gray-900">
+        {/* Navigation Bar with hash links */}
+        <Navigation>
+          {/* Example hash links */}
+          <a href="#hero">Home</a>
+          <a href="#about">About</a>
+          <a href="#features">Features</a>
+          <a href="#breeds">Breeds</a>
+          <a href="#stats">Stats</a>
+          <a href="#testimonials">Testimonials</a>
+          <a href="#newsletter">Newsletter</a>
+        </Navigation>
 
-      {/* Header (optional hero tagline or callout) */}
-      <Header />
+        {/* Header */}
+        <Header />
 
-      {/* Main Content */}
-      <main className="flex-grow">
-        {/* Hero Section */}
-        <section id="hero">
-          <Hero />
-        </section>
+        {/* Main Content */}
+        <main className="flex-grow">
+          <section id="hero">
+            <Hero />
+          </section>
 
-        {/* About Section */}
-        <section id="about" className="py-16">
-          <About />
-        </section>
+          <section id="about" className="py-16">
+            <About />
+          </section>
 
-        {/* Farm Features */}
-        <section id="features" className="py-16 bg-white">
-          <Features />
-        </section>
+          <section id="features" className="py-16 bg-white">
+            <Features />
+          </section>
 
-        {/* Chicken Breeds Showcase */}
-        <section id="breeds" className="py-16 bg-gray-100">
-          <ProductShowcase />
-        </section>
+          <section id="breeds" className="py-16 bg-gray-100">
+            <ProductShowcase />
+          </section>
 
-        {/* Poultry Farm Stats */}
-        <section id="stats" className="py-16 bg-white">
-          <Stats />
-        </section>
+          <section id="stats" className="py-16 bg-white">
+            <Stats />
+          </section>
 
-        {/* Testimonials */}
-        <section id="testimonials" className="py-16 bg-gray-100">
-          <Testimonials />
-        </section>
+          <section id="testimonials" className="py-16 bg-gray-100">
+            <Testimonials />
+          </section>
 
-        {/* Newsletter */}
-        <section id="newsletter" className="py-16 bg-white">
-          <Newsletter />
-        </section>
-      </main>
+          <section id="newsletter" className="py-16 bg-white">
+            <Newsletter />
+          </section>
+        </main>
 
-      {/* Footer */}
-      <Footer />
-    </div>
+        {/* Footer */}
+        <Footer />
+      </div>
+    </HashRouter>
   );
 }
 
 export default App;
+
 
